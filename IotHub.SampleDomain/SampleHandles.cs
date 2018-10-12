b1950ecf6c71314f5f9879e6c97cf31e1b42e06f
badpaybad <badpaybad@gmail.com>
2018-10-12 07:06:31 +0700

    guide for build api -> send command by swagger UI -> check console invoke pubsub event

--- IotHub.SampleDomain/SampleHandles.cs
+++ IotHub.SampleDomain/SampleHandles.cs
@@ -1,6 +1,7 @@
 ﻿using IotHub.CommandsEvents.SampleDomain;
 using IotHub.Core.Cqrs;
 using IotHub.Core.Cqrs.EventSourcingRepository;
+using IotHub.Core.CqrsEngine;
 using Newtonsoft.Json;
 using System;
 
@@ -27,6 +28,12 @@ namespace IotHub.SampleDomain
             //}
             Console.WriteLine("Call from api but Exec in console. Saved to db:" );
             Console.WriteLine(temp);
+
+            CommandEventSender.Send(new SampleEventCreated()
+            {
+                PublishedEventId = Guid.NewGuid(),
+                SampleVersion = $"Come from CreateSample command with cmdid {c.PublishedCommandId}"
+            });
         }
     }
 }
